5fe87c04606da95d5398f0e773bca24af235c565
Meenakshi312 <meenu20043@gmail.com>
2025-09-04 15:27:11 +0530

    i

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import notesRouter from "./routes/notes.js"
 import dotenv from 'dotenv';
 const app=express();
 dotenv.config();
-const PORT= process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 || 'https://mini-to-do-app-ysni.vercel.app/';
 const DATABASE_URl = process.env.DATABASE_URl || "mongodb://localhost:27017/test"
 
 app.get('/', (req,res)=>{
